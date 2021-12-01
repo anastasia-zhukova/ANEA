054bdd658c46f056567ce85a593ee409ba8ad015
zakaria irkha <zakariairkha@gmail.com>
2021-12-01 13:31:11 +0100

    updated homePAge.js

--- annotation-tool/src/components/HomePage.js
+++ annotation-tool/src/components/HomePage.js
@@ -8,11 +8,14 @@ const HomePage = () => {
     return (
         
         <>
-            <section className="welcomeSec container">
+            <section className="welcomeSec ">
                 
                 <div >
                     <h1 >The tool to train your datasets</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <div className="btnContainer">
+                        <button>Get started</button>
+                    </div>
                 </div>
                 <img src={datasetPic} alt="dataset" />
                 
